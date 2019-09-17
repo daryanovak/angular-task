38a91e96dc033f2e3a22296859c5c393c13b776d
darya <daryanovak18@gmail.com>
2019-09-17 13:22:12 +0300

    init: project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { BoardComponent } from './board/board.component';
+import { FooterComponent } from './footer/footer.component';
+import { CardListComponent } from './board/card-list/card-list.component';
+import { CardItemComponent } from './board/card-list/card-item/card-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ToolbarComponent,
+    BoardComponent,
+    FooterComponent,
+    CardListComponent,
+    CardItemComponent
   ],
   imports: [
     BrowserModule
